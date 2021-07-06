b7e85618c1c267712f0b6b4432ca5bc52f8f9e40
Nick Celesti <celesti.3@buckeyemail.osu.edu>
2021-07-06 18:43:07 -0400

    finished assignment 1

---
+++ Assignment1.java
@@ -0,0 +1,44 @@
+// Nick Celesti
+
+class Assignment1 {
+    public static void main(String[] args) {
+        // -----------------------------------
+        System.out.println("1)");
+        String stars = "****";
+        String spaces = "";
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(stars.substring(0, i)); // substring does not alter the string itself
+        }
+        // -----------------------------------
+        System.out.println(".........\n2)\n..........");
+
+        stars = "****";
+        spaces = "";
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(stars.substring(i));
+        }
+
+        // -----------------------------------
+        System.out.println("3)");
+
+        spaces = "     ";
+        stars = "*";
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(spaces.substring(i) + stars);
+            stars += "**"; // concatenate 2 stars
+        }
+        // -----------------------------------
+        System.out.println("...........\n4)\n............");
+
+        stars = "*********";
+        spaces = "     ";
+
+        for (int i = 2; i < 6; i++) {
+            stars = stars.substring(2); // remove 2 stars
+            System.out.println(spaces.substring(0, i) + stars);
+        }
+
+    }
+}
