11cfc682aa6a33fa74a3e64bf0e08950cc8b903d
Nick Celesti <celesti.3@buckeyemail.osu.edu>
2021-07-21 13:20:53 -0400

    fixed rdmbs_7 (hopefully)

--- RDBMS/RDBMS_7.sql
+++ RDBMS/RDBMS_7.sql
@@ -1,4 +1,6 @@
 SELECT title, noOfCopies
-FROM   (((tbl_author NATURAL JOIN tbl_book) NATURAL JOIN tbl_book_copies) NATURAL JOIN tbl_library_branch)
+FROM (((tbl_book inner join tbl_author on authId = authorId) inner join 
+tbl_book_copies on tbl_book.bookId = tbl_book_copies.bookId) inner join 
+tbl_library_branch on tbl_book_copies.branchId = tbl_library_branch.branchId)
 WHERE authorName='Stephen King' AND 
 BranchName='Central'; 
